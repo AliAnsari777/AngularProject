f1fc6c45d2aace11101a97f68747d48adcc22d11
ALi Ansari <alidk2013@gmail.com>
2020-11-02 23:27:28 -0700

    toggeling for recipe and shopping cart added in header and app components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  // the @output() annotation enable us to listen to the "featureSelected" in parent component "app-component".
+  @Output() featureSelected = new EventEmitter<String>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
