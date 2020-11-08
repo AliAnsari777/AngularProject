f9884ff7d303637d9f2f1a387f498224b7636fca
ALi Ansari <alidk2013@gmail.com>
2020-11-07 21:15:45 -0700

    custom directive added

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ContentChild, ElementRef, OnInit, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredients } from 'src/app/shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput', { static: false }) inputName: ElementRef;
+  @ViewChild('amountInput', { static: false }) inputAmount: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredients>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addNewItem() {
+    const ingName = this.inputName.nativeElement.value;
+    const ingAmount = this.inputAmount.nativeElement.value;
+    const ingredient = new Ingredients(ingName, ingAmount);
+    this.ingredientAdded.emit(ingredient);
   }
 
 }
